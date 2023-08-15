29539107da440ba8c6f6a00a0aca7983fff7ce09
Leonardo Ferigutti <leonardoferigutti@gmail.com>
2023-08-15 13:59:24 +0200

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,16 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Implementation made by Leonardo */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n == 1){
+            return  n;
+        }
+        else if (n%2 == 1) {
+            return 3*n + 1;
+        }
+        else {
+            return n/2;
         }
     }
 
