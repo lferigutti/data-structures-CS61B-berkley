b34cdf4b3162a4d209ab12227e7dc3fbf3d8094d
Leonardo Ferigutti <leonardoferigutti@gmail.com>
2023-08-23 08:47:34 +0200

    lab2 solved

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,40 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimeEndBeg(){
+        IntList lst_prime_end = IntList.of(14,15,17);
+        IntList lst_prime_beg = IntList.of(17,15,16);
+        boolean changed_end = IntListExercises.squarePrimes(lst_prime_end);
+        boolean changed_beg = IntListExercises.squarePrimes(lst_prime_beg);
+
+        assertEquals("14 -> 15 -> 289", lst_prime_end.toString());
+        assertEquals("289 -> 15 -> 16", lst_prime_beg.toString());
+        assertTrue(changed_end);
+        assertTrue(changed_beg);
+    }
+
+    @Test
+    public void testSquarePrimeNoChange(){
+        IntList lst = IntList.of(14,15,18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 18", lst.toString());
+        assertFalse(changed);
+
+    }
+    @Test
+    public void testSquarePrimeComplex(){
+        IntList lst_prime_end = IntList.of(1,17,3);
+        IntList lst_prime_beg = IntList.of(10,17,0);
+        boolean changed_end = IntListExercises.squarePrimes(lst_prime_end);
+        boolean changed_beg = IntListExercises.squarePrimes(lst_prime_beg);
+
+        assertEquals("1 -> 289 -> 9", lst_prime_end.toString());
+        assertEquals("10 -> 289 -> 0", lst_prime_beg.toString());
+        assertTrue(changed_end);
+        assertTrue(changed_beg);
+    }
+
+
 }
